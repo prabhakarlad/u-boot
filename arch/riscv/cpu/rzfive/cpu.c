17109486be9437333e3b82e0d7fc6811f7c159f7
Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
2022-10-28 00:43:44 +0100

    RZ/Five fixes for upstream FDT OpenSBI
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>

--- arch/riscv/cpu/rzfive/cpu.c
+++ arch/riscv/cpu/rzfive/cpu.c
@@ -9,6 +9,31 @@
 #include <cpu_func.h>
 #include <irq_func.h>
 #include <asm/cache.h>
+#include <asm/csr.h>
+
+#define CSR_MCACHE_CTL		0x7ca
+#define CSR_MMISC_CTL		0x7d0
+#define CSR_MARCHID		0xf12
+
+#define V5_MCACHE_CTL_IC_EN_OFFSET		0
+#define V5_MCACHE_CTL_DC_EN_OFFSET		1
+#define V5_MCACHE_CTL_CCTL_SUEN_OFFSET		8
+#define V5_MCACHE_CTL_L1I_PREFETCH_OFFSET	9
+#define V5_MCACHE_CTL_L1D_PREFETCH_OFFSET	10
+#define V5_MCACHE_CTL_DC_WAROUND_OFFSET_1	13
+#define V5_MCACHE_CTL_DC_WAROUND_OFFSET_2	14
+#define V5_MCACHE_CTL_DC_COHEN_OFFSET		19
+#define V5_MCACHE_CTL_DC_COHSTA_OFFSET		20
+
+#define V5_MCACHE_CTL_IC_EN		BIT(V5_MCACHE_CTL_IC_EN_OFFSET)
+#define V5_MCACHE_CTL_DC_EN		BIT(V5_MCACHE_CTL_DC_EN_OFFSET)
+#define V5_MCACHE_CTL_CCTL_SUEN		BIT(V5_MCACHE_CTL_CCTL_SUEN_OFFSET)
+#define V5_MCACHE_CTL_DC_COHEN_EN	BIT(V5_MCACHE_CTL_DC_COHEN_OFFSET)
+#define V5_MCACHE_CTL_L1I_PREFETCH_EN	BIT(V5_MCACHE_CTL_L1I_PREFETCH_OFFSET)
+#define V5_MCACHE_CTL_L1D_PREFETCH_EN	BIT(V5_MCACHE_CTL_L1D_PREFETCH_OFFSET)
+#define V5_MCACHE_CTL_DC_WAROUND_1_EN	BIT(V5_MCACHE_CTL_DC_WAROUND_OFFSET_1)
+#define V5_MCACHE_CTL_DC_WAROUND_2_EN	BIT(V5_MCACHE_CTL_DC_WAROUND_OFFSET_2)
+#define V5_MCACHE_CTL_DC_COHSTA_EN	BIT(V5_MCACHE_CTL_DC_COHSTA_OFFSET)
 
 /*
  * cleanup_before_linux() is called just before we call linux
@@ -26,4 +51,26 @@ int cleanup_before_linux(void)
 	dcache_disable();
 
 	return 0;
+}
+
+void harts_early_init(void)
+{
+	if (CONFIG_IS_ENABLED(RISCV_MMODE)) {
+		unsigned long long mcache_ctl_val = csr_read(CSR_MCACHE_CTL);
+
+		/* enable L1 cache */
+		if (!(mcache_ctl_val & V5_MCACHE_CTL_IC_EN))
+			mcache_ctl_val |= V5_MCACHE_CTL_IC_EN;
+		if (!(mcache_ctl_val & V5_MCACHE_CTL_DC_EN))
+			mcache_ctl_val |= V5_MCACHE_CTL_DC_EN;
+		if (!(mcache_ctl_val & V5_MCACHE_CTL_CCTL_SUEN))
+			mcache_ctl_val |= V5_MCACHE_CTL_CCTL_SUEN;
+		if (!(mcache_ctl_val & V5_MCACHE_CTL_L1I_PREFETCH_EN))
+			mcache_ctl_val |= V5_MCACHE_CTL_L1I_PREFETCH_EN;
+		if (!(mcache_ctl_val & V5_MCACHE_CTL_L1D_PREFETCH_EN))
+			mcache_ctl_val |= V5_MCACHE_CTL_L1D_PREFETCH_EN;
+		mcache_ctl_val &= ~(V5_MCACHE_CTL_DC_WAROUND_1_EN | V5_MCACHE_CTL_DC_WAROUND_2_EN);
+		mcache_ctl_val |= V5_MCACHE_CTL_DC_WAROUND_1_EN;
+		csr_write(CSR_MCACHE_CTL, mcache_ctl_val);
+	}
 }
